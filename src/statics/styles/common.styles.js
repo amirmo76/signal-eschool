257b7a918d227e70d2badc7412d8f029e1cc06fa
Amir Mohseni <amir.mohseni7697@gmail.com>
2021-07-20 20:27:41 +0430

    added common styles

---
+++ src/statics/styles/common.styles.js
@@ -0,0 +1,11 @@
+import { css } from 'styled-components';
+import hexToRgb from '../../utils/hex-to-rgb';
+import colors from '../values/colors.json';
+
+export const BorderCSS = css`
+  border: 1px solid rgba(${hexToRgb(colors.primary)}, 0.1);
+`;
+
+export const ShadowCSS = css`
+  box-shadow: rgba(${hexToRgb(colors.primary)}, 0.23) 0 1.5rem 2.5rem;
+`;
