614d5bd062783feb6d84f3720d98af970194a3d8
Amir Mohseni <amir.mohseni7697@gmail.com>
2021-08-01 10:45:02 +0430

    refactored browser router

--- src/components/containers/index/index.jsx
+++ src/components/containers/index/index.jsx
@@ -5,17 +5,17 @@ import {
   Switch,
   useRouteMatch,
   Link,
-} from "react-router-dom";
+} from 'react-router-dom';
 // Components
-import HeadingOne from "../../atoms/heading-1";
-import LoginForm from "../../organisms/login-form";
-import SignupForm from "../../organisms/signup-form";
+import HeadingOne from '../../atoms/heading-1';
+import LoginForm from '../../organisms/login-form';
+import SignupForm from '../../organisms/signup-form';
 // Statics
-import faLang from "../../../statics/values/langs/fa.json";
-import { ReactComponent as HeroSVG } from "../../../statics/svgs/hero.svg";
-import { ReactComponent as Circle } from "../../../statics/svgs/main-circle.svg";
+import faLang from '../../../statics/values/langs/fa.json';
+import { ReactComponent as HeroSVG } from '../../../statics/svgs/hero.svg';
+import { ReactComponent as Circle } from '../../../statics/svgs/main-circle.svg';
 // Styles
-import { Header, Description, RightWrapper, Wrapper, Options } from "./styles";
+import { Header, Description, RightWrapper, Wrapper, Options } from './styles';
 
 function Index() {
   let { path, url } = useRouteMatch();
@@ -25,43 +25,41 @@ function Index() {
       <HeroSVG className="hero" />
       <Circle className="circle" />
       <RightWrapper>
-        <BrowserRouter>
-          <Switch>
-            <Route path={`${path}signup`} exact>
-              <Header>
-                <HeadingOne>{faLang.signup.title}</HeadingOne>
-              </Header>
-              <SignupForm className="signup_form" />
-              <Options>
-                <div className="signup">
-                  <div className="question">{faLang.login.question}</div>
-                  <Link className="link" to={`${url}`}>
-                    {faLang.login.link}
-                  </Link>
-                </div>
-              </Options>
-            </Route>
-            <Route exact path={path}>
-              <Header>
-                <HeadingOne>{faLang.main.title}</HeadingOne>
-                <Description>{faLang.main.description}</Description>
-              </Header>
-              <LoginForm />
-              <Options>
-                <div className="signup">
-                  <div className="question">{faLang.signup.question}</div>
-                  <Link className="link" to={`${url}signup`}>
-                    {faLang.signup.link}
-                  </Link>
-                </div>
-                <div className="link">{faLang.forgot_password}</div>
-              </Options>
-            </Route>
-            <Route>
-              <HeadingOne>{faLang.not_found}</HeadingOne>
-            </Route>
-          </Switch>
-        </BrowserRouter>
+        <Switch>
+          <Route path={`${path}signup`} exact>
+            <Header>
+              <HeadingOne>{faLang.signup.title}</HeadingOne>
+            </Header>
+            <SignupForm className="signup_form" />
+            <Options>
+              <div className="signup">
+                <div className="question">{faLang.login.question}</div>
+                <Link className="link" to={`${url}`}>
+                  {faLang.login.link}
+                </Link>
+              </div>
+            </Options>
+          </Route>
+          <Route exact path={path}>
+            <Header>
+              <HeadingOne>{faLang.main.title}</HeadingOne>
+              <Description>{faLang.main.description}</Description>
+            </Header>
+            <LoginForm />
+            <Options>
+              <div className="signup">
+                <div className="question">{faLang.signup.question}</div>
+                <Link className="link" to={`${url}signup`}>
+                  {faLang.signup.link}
+                </Link>
+              </div>
+              <div className="link">{faLang.forgot_password}</div>
+            </Options>
+          </Route>
+          <Route>
+            <HeadingOne>{faLang.not_found}</HeadingOne>
+          </Route>
+        </Switch>
       </RightWrapper>
     </Wrapper>
   );
