f7f69046fb5918246a9e02617c445a324ae94a7c
Amir Mohseni <amir.mohseni7697@gmail.com>
2021-08-01 10:45:18 +0430

    added login change route for demo

--- src/components/organisms/login-form/login-form.component.jsx
+++ src/components/organisms/login-form/login-form.component.jsx
@@ -1,15 +1,20 @@
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 
-import Button from "../../atoms/button";
-import TextField from "../../atoms/text-field";
-import faLang from "../../../statics/values/langs/fa.json";
+import Button from '../../atoms/button';
+import TextField from '../../atoms/text-field';
+import faLang from '../../../statics/values/langs/fa.json';
 
-import { StyledLoginForm } from "./login-form.styles";
+import { StyledLoginForm } from './login-form.styles';
 
 function LoginForm(props) {
   const { handleSubmit, register } = useForm();
+  const history = useHistory();
 
-  const onLoginSubmit = (data) => console.log(data);
+  const onLoginSubmit = (data) => {
+    console.log(data);
+    history.push('/panel/dashboard');
+  };
 
   return (
     <StyledLoginForm onSubmit={handleSubmit(onLoginSubmit)} {...props}>
@@ -17,13 +22,13 @@ function LoginForm(props) {
         id="username"
         type="text"
         label={faLang.username}
-        formRef={register("username", { required: true })}
+        formRef={register('username', { required: true })}
       />
       <TextField
         id="password"
         type="password"
         label={faLang.password}
-        formRef={register("password", { required: true })}
+        formRef={register('password', { required: true })}
       />
       <Button label={faLang.login.button} isLoading={false} expanded />
     </StyledLoginForm>
