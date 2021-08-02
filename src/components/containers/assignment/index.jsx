051776ec36c6bde77ad1436909e128db24bb41df
Amir Mohseni <amir.mohseni7697@gmail.com>
2021-08-02 13:19:19 +0430

    added item-info component

--- src/components/containers/assignment/index.jsx
+++ src/components/containers/assignment/index.jsx
@@ -1,7 +1,7 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import Card from '../../atoms/card';
 import Section from '../../molecules/section';
-import InfoList from '../../atoms/info-list';
+import ItemInfo from '../../organisms/item-info';
 import RichText from '../../atoms/rich-text';
 import List from '../../atoms/list';
 import Attachment from '../../molecules/attachment';
@@ -13,42 +13,11 @@ import faLang from '../../../statics/values/langs/fa.json';
 function Index() {
   const [assignment] = useState(assignmentData);
 
-  const infoItems = useMemo(
-    () => [
-      {
-        id: 0,
-        title: <span>{faLang.status}</span>,
-        value: (
-          <span
-            className={assignment.status === 'active' ? 'success' : 'danger'}
-          >
-            {assignment.status === 'active' ? faLang.active : faLang.not_active}
-          </span>
-        ),
-      },
-      {
-        id: 1,
-        title: <span>{faLang.due}</span>,
-        value: <span>{assignment.due.text}</span>,
-      },
-      {
-        id: 2,
-        title: <span>{faLang.grade}</span>,
-        value: (
-          <span>{`${
-            assignment.grade.extra ? `(${assignment.grade.extra}+) ` : ''
-          }${assignment.grade.max - (assignment.grade.extra || 0)}`}</span>
-        ),
-      },
-    ],
-    [assignment]
-  );
-
   return (
     <div>
       <Card>
         <Section title={assignment.title}>
-          <InfoList items={infoItems} />
+          <ItemInfo {...assignment} />
         </Section>
         <Section title={faLang.description}>
           <RichText value={assignment.body} />
---
+++ src/components/organisms/item-info/item-info.component.jsx
@@ -0,0 +1,57 @@
+import { useMemo } from 'react';
+import InfoList from '../../atoms/info-list';
+import { getGradeColor } from '../../../utils/grade';
+
+import faLang from '../../../statics/values/langs/fa.json';
+
+function ItemInfo({ status, due, grade }) {
+  const infoItems = useMemo(() => {
+    const items = [];
+    // add status
+    items.push({
+      id: 0,
+      title: <span>{faLang.status}</span>,
+      value: (
+        <span className={status === 'active' ? 'success' : 'danger'}>
+          {status === 'active' ? faLang.active : faLang.not_active}
+        </span>
+      ),
+    });
+    // add due text
+    items.push({
+      id: 1,
+      title: <span>{faLang.due}</span>,
+      value: <span>{due.text}</span>,
+    });
+    // add max grade
+    items.push({
+      id: 2,
+      title: <span>{faLang.grade}</span>,
+      value: (
+        <span>{`${grade.extra ? `(${grade.extra}+) ` : ''}${
+          grade.max - (grade.extra || 0)
+        }`}</span>
+      ),
+    });
+    // add your grade
+    if (grade.current)
+      items.push({
+        id: 2,
+        title: <span>{faLang.my_grade}</span>,
+        value: (
+          <span style={{ color: getGradeColor(grade.current) }}>
+            {grade.current}
+          </span>
+        ),
+      });
+    return items;
+  }, [status, due, grade]);
+
+  return (
+    <div>
+      <InfoList items={infoItems} />
+    </div>
+  );
+}
+
+export default ItemInfo;
