b7e4c3d1e0121e3be00c49efedd0af588a7d3924
Amir Mohseni <amir.mohseni7697@gmail.com>
2021-08-04 11:59:09 +0430

    updated quiz to use countdown

--- src/components/containers/quiz/index.jsx
+++ src/components/containers/quiz/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import dayjs from 'dayjs';
 import Card from '../../atoms/card';
 import Section from '../../molecules/section';
 import ItemInfo from '../../organisms/item-info';
@@ -6,25 +7,26 @@ import RichText from '../../atoms/rich-text';
 import List from '../../atoms/list';
 import Attachment from '../../molecules/attachment';
 import SubmissionCard from '../../organisms/submission-card';
-import projectData from './dummy.data.json';
+import Countdown from '../../atoms/countdown';
+import quizData from './dummy.data.json';
 
 import faLang from '../../../statics/values/langs/fa.json';
 
 function Index() {
-  const [project] = useState(projectData);
-
+  const [quiz] = useState(quizData);
   return (
     <div>
       <Card>
-        <Section title={project.title}>
-          <ItemInfo {...project} />
+        <Countdown startUnix={dayjs(quiz.due).unix()} />
+        <Section title={quiz.title}>
+          <ItemInfo {...quiz} />
         </Section>
         <Section title={faLang.description}>
-          <RichText value={project.body} />
+          <RichText value={quiz.body} />
         </Section>
         <Section title={faLang.attachments}>
           <List
-            items={project.attachments.map((cur) => (
+            items={quiz.attachments.map((cur) => (
               <Attachment {...cur} />
             ))}
           />
@@ -32,7 +34,7 @@ function Index() {
         <Section title={faLang.submission_list}>
           {
             <List
-              items={project.submissions.map((cur) => (
+              items={quiz.submissions.map((cur) => (
                 <SubmissionCard {...cur} />
               ))}
             />
