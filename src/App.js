3a1fc7e19e52466ff33cadac309e449e6bc1bbbf
Amir Mohseni <amir.mohseni7697@gmail.com>
2021-08-04 11:57:32 +0430

    added dayjs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import HomepageContainer from "./components/containers/index";
-import PanelContainer from "./components/containers/panel";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import dayjs from 'dayjs';
+import jalaliday from 'jalaliday';
+
+import HomepageContainer from './components/containers/index';
+import PanelContainer from './components/containers/panel';
 
 function App() {
+  dayjs.extend(jalaliday);
   return (
     <div className="App">
       <BrowserRouter>
